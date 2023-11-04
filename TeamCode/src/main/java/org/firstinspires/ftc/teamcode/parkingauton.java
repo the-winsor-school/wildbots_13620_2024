946f42da8ab88598292086f3094a387cc15a3764
SusanZuo23 <tszhuen.zuo@winsor.edu>
2023-11-04 11:56:55 -0400

    auton test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkingauton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkingauton.java
@@ -2,25 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.teamcode.driving.IDriving;
 
 @Autonomous(name="parking")
 public class parkingauton extends LinearOpMode{
     Robot robot;
-    LinearOpMode opMode;
-
+    public ColorSensor color;
+    color.red;
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(opMode);
+        robot = new Robot(this);
 
     waitForStart();
 
         if (opModeIsActive()) {
             while (!robot.checkTape() && opModeIsActive()) {
-                robot.driving.vertical(1);
-                sleep(300);
+                telemetry.addData("no","Tape");
+                telemetry.addData("red", color.red());
+                telemetry.update();
+                robot.driving.vertical(0.5f);
             }
-            robot.driving.stop();
+                robot.driving.stop();
         }
     }
 }
