596b103c7b60bd082432958e05b0cbd4a0abce4f
kenna26 <90514808+kenna26@users.noreply.github.com>
2023-11-04 10:58:30 -0400

    arm class and strting objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,7 +1,9 @@
+/*
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.driving.GridDrive;
@@ -17,6 +19,13 @@ public class Robot {
     public DcMotor lf;
     public DcMotor lb;
 
+    //arm
+    public DcMotor bottomMotor;
+    public DcMotor topMotor;
+
+    public Servo rightServo;
+    public Servo leftServo;
+
     public IDriving driving;
 
 
@@ -29,7 +38,15 @@ public class Robot {
         lf = map.tryGet(DcMotor.class, "lf");
         lb = map.tryGet(DcMotor.class, "lb");
 
+        //arm
+        bottomMotor = map.tryGet(DcMotor.class, "bottom arm joint");
+        topMotor = map.tryGet(DcMotor.class, "top arm joint");
+
+        rightServo = map.tryGet(Servo.class, "right servo");
+        leftServo = map.tryGet(Servo.class, "left servo");
+
         driving = new StrafeDrive(rf, rb, lf, lb);
     }
 
 }
+*/
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/arm.java
@@ -0,0 +1,124 @@
+/*
+package org.firstinspires.ftc.teamcode.arm;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.*;
+
+public class arm {
+
+    public ArmJoint bottomJoint;
+    public ArmJoint topJoint;
+
+    public Claw claw;
+
+    double armPower = 0.5;
+    int tolerance = 100;
+
+    public arm(Robot robot) {
+        bottomJoint = new ArmJoint(robot.bottomMotor, 0);
+        topJoint = new ArmJoint(robot.topMotor, 0);
+
+        claw = new Claw(robot.rightServo, robot.leftServo);
+
+    }
+
+    public class ArmJoint {
+        int gearRatio;
+        double powerUsed;
+        DcMotor motor;
+        int armTolerance;
+
+
+        public ArmJoint(DcMotor motor, int gearRatio) {
+            this.motor = motor;
+            this.gearRatio = gearRatio;
+            powerUsed = armPower;
+            armTolerance = tolerance;
+        }
+
+        public void resetEncoders() {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+
+        public void moveArm(int targetPosition) { //targetPosition in rotation
+            Thread moveArm = new MoveArm(targetPosition);
+            moveArm.start();
+        }
+
+        public int getCurrentPosition() {
+            return motor.getCurrentPosition();
+        }
+
+        public void setMotorPower(double power) {
+            motor.setPower(power);
+        }
+
+        public void brake() {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        public void changePower(int power) {
+            powerUsed += power;
+        }
+
+        public class MoveArm extends Thread {
+            private int targetPosition;
+
+            public MoveArm(int targetPosition) {
+                this.targetPosition = targetPosition;
+            }
+
+            public void run() {
+                while (Math.abs(getCurrentPosition() - targetPosition) > armTolerance) {//motor is high
+                    motor.setTargetPosition(targetPosition);
+                    motor.setPower(powerUsed);
+                    motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public class Claw {
+        Servo right;
+        Servo left;
+
+        public Claw(Servo rightServo, Servo leftServo) {
+            right = rightServo;
+            left = leftServo;
+        }
+
+        public void clawControls(ClawPos pos) {
+            switch (pos) {
+                case OPEN:
+                    right.setPower(-1);
+                    left.setPower(1);
+                    break;
+
+                case STOP:
+                    right.setPower(0);
+                    left.setPower(0);
+                    break;
+
+                case CLOSE:
+                    right.setPower(1);
+                    left.setPower(-1);
+                    break;
+            }
+        }
+    }
+
+    public enum ClawPos {
+        OPEN,
+        CLOSE,
+        STOP
+    }
+
+}
+*/
