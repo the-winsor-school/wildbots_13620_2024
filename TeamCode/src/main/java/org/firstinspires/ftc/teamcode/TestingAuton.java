bf944bf3e153c2cd82a0517860c58233f49bfaab
kenna26 <90514808+kenna26@users.noreply.github.com>
2023-11-18 13:12:24 -0500

    reformat files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAuton.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.driving.*;
 
 @Autonomous(name="test auto")
-public class SimpleAuton extends LinearOpMode {
+public class TestingAuton extends LinearOpMode {
     Robot robot;
     LinearOpMode opMode;
     IDriving driving;
