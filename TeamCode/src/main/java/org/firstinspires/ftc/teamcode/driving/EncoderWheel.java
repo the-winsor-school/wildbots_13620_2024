d864cafb0ded7c86669038e5901dafb65ff186df
kenna26 <90514808+kenna26@users.noreply.github.com>
2023-12-11 11:21:14 -0500

    knital

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driving/EncoderWheel.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.driving;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.Vector;
+
+public class EncoderWheel {
+
+    DcMotor motor;
+
+    int ticksPerRev;
+
+    public EncoderWheel(DcMotor motor) {
+        this.motor = motor;
+    }
+
+    public Vector getVelocity()
+
+}
