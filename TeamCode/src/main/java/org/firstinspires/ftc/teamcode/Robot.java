3c426d4e349df7b48823f93deb4976b1d3ae5cd2
Jason Cox <coxj@vt.edu>
2024-01-19 18:16:43 -0500

    nora took mr cox's computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -5,8 +5,10 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Arm.*;
 import org.firstinspires.ftc.teamcode.driving.IDriving;
 import org.firstinspires.ftc.teamcode.driving.StrafeDrive;
@@ -60,6 +62,10 @@ public class Robot {
 
     private LinearOpMode opMode;
 
+    private DistanceSensor leftDistance;
+    private DistanceSensor frontDistance;
+    private DistanceSensor rightDistance;
+
     /**
      * @param opMode pass by writing: new Robot(this);
      */
@@ -98,6 +104,10 @@ public class Robot {
         driving = new StrafeDrive(rf, rb, lf, lb);
 
         arm = new FullArm(elbowMotor, elbowLimit, wristMotor, wristLimit, wristPotentiometer, rightServo, leftServo);
+
+        leftDistance = map.tryGet(DistanceSensor.class, "leftDistance");
+        rightDistance = map.tryGet(DistanceSensor.class,"rightDistance");
+        frontDistance = map.tryGet(DistanceSensor.class,"frontDistance");
     }
 
     public void printWheelPowers() {
@@ -139,4 +149,15 @@ public class Robot {
         return wristLimit.isPressed();
     }
 
+    public double  getFrontDistance() {
+        return frontDistance.getDistance(DistanceUnit.CM);
+    }
+
+    public double getLeftDistance() {
+        return leftDistance.getDistance(DistanceUnit.CM);
+    }
+
+    public double getRightDistance() {
+        return rightDistance.getDistance(DistanceUnit.CM);
+    }
 }
